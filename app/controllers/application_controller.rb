a1d04bcaad2da62cf01d59643b3102af89f1225a
saopen777 <saopen@icloud.com>
2020-08-26 12:57:04 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
 
   before_action :basic_auth
   before_action :authenticate_with_http_digest
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, format: { with: VALID_EMAIL_REGEX}
     validates :email, uniqueness: true
-    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "Include both letters and numbers"}
-    validates :password_confirmation, format: { with: /\A[a-z0-9]+\z/i, message: "Include both letters and numbers"}
+    validates :password, format: { with: /\A[a-z0-9]+\z{6,}/i, message: "Include both letters and numbers"}
+    validates :password_confirmation, format: { with: /\A[a-z0-9]+\z{6,}/i, message: "Include both letters and numbers"}
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: " Full-width characters"}
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
