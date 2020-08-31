ab1aa3381dfa0c18a5b18635761afcf7c3adee85
saopen777 <saopen@icloud.com>
2020-08-31 21:28:23 +0900

    訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,6 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  VALID_PRICE_REGEX = /\A[0-9]+\z/
-  validates :price, format: { with: VALID_PRICE_REGEX}
+  validates :price, format: { with: /\A[0-9]+\z/ }
 
 end
